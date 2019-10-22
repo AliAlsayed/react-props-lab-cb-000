1b207d8108e4af33d967cd5123b57e4f311cfc1a
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-22 08:41:04 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,3 +1,11 @@
 // Code The Spaceship Component Here
 
 import React from 'react';
+
+export default class Spaceship extends React.Component{
+  render(){
+    return (
+      <h1></h1>
+    )
+  }
+}
