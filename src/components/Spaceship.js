c424408562c9d3370a6ec77daf89ffc5a562e453
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-22 08:43:32 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -8,6 +8,7 @@ export default class Spaceship extends React.Component{
       <h1>{this.props.name}</h1>
       <p>{this.props.speed}</p>
       <p>{this.props.hasRockets}</p>
+      <p>{this.props.colors}</p>
     )
   }
 }
